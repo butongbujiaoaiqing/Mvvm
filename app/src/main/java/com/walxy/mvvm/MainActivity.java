def55397dfa5dfc2a3129ffc4a2b6781e40efbda
SH <1450497718@qq.com>
2017-10-31 15:59:25 +0800

    MVVM模式

---
+++ app/src/main/java/com/walxy/mvvm/MainActivity.java
@@ -0,0 +1,28 @@
+package com.walxy.mvvm;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.walxy.mvvm.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        UserEntity userEntity = new UserEntity();
+
+        userEntity.setAge(21);
+        userEntity.setNickname("lufei");
+        userEntity.setUsername("路飞");
+        userEntity.setImage("https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=366589626,1283096638&fm=27&gp=0.jpg");
+
+        mainBinding.setUser(userEntity);
+
+    }
+}
